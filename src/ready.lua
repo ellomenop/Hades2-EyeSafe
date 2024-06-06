190655d44faf26d97b9435b84fc7a712fd00901c
ellomenop <ellomenop.speedruns@gmail.com>
2024-06-06 01:09:47 -0400

    Initial files

---
+++ src/ready.lua
@@ -0,0 +1,16 @@
+---@meta _
+-- globals we define are private to our plugin!
+---@diagnostic disable: lowercase-global
+
+-- here is where your mod sets up all the things it will do.
+-- this file will not be reloaded if it changes during gameplay
+-- 	so you will most likely want to have it reference
+--	values and functions later defined in `reload.lua`.
+-- Absolute Path links to plugins_data folder - mod folder, then package name - which MUST contain mod author name for uniqueness
+local package = rom.path.combine(_PLUGIN.plugins_data_mod_folder_path, "ellomenop-EyeSafe")
+-- Example Output of Package Path:
+-- C:\Program Files (x86)\Steam\steamapps\common\Hades II\Ship\ReturnOfModding\plugins_data\zannc-KeepsakePort\zannc-KeepsakePort
+modutil.mod.Path.Wrap("SetupMap", function(base)
+    LoadPackages({Name = package})
+    base()
+end)
---
+++ src/sjson.lua
@@ -0,0 +1,26 @@
+---@meta _
+---@diagnostic disable
+
+-- local sjson = rom.mods['SGG_Modding-SJSON']
+-- local gui_animation_path = rom.path.combine(rom.paths.Content, 'Game/Animations/GUIAnimations.sjson')
+
+-- local icon_order = {
+--     'Name',
+--     'InheritFrom',
+--     'FilePath',
+-- }
+
+-- sjson.hook(gui_animation_path, function(data)
+-- 	for index, animation_data in ipairs(data.Animations) do
+-- 		if animation_data.Name == "Keepsake_31" then
+-- 			animation_data.FilePath = "ellomenop-eyesafe\\NemesisReward"
+-- 		end
+-- 	end
+-- 	-- game.TraitData.DeathVengeanceKeepsake.Icon = "Keepsake_31",
+
+-- 	-- {
+-- 	-- 	Name = "Keepsake_31"
+-- 	-- 	InheritFrom = "KeepsakeIcon"
+-- 	-- 	FilePath = "GUI\Screens\AwardMenu\Keepsake_31"
+-- 	-- }
+-- end)
